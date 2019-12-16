46d5ede1ce408523b6469c0230ef706526e5549d
Anton Samoshin <anton.samoshin@norbit.ru>
2019-12-16 17:08:22 +0300

    Added react-router-dom and menu items navigation

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,28 +13,33 @@ class Directory extends React.Component {
                 title: 'CPU',
                 imageUrl: 'https://3dnews.ru/assets/external/illustrations/2019/01/15/981058/asdf1.jpg',
                 id: 1,
+                linkUrl: 'cpu',
             },
             {
                 title: 'Motherboards',
                 imageUrl: 'https://www.asus.com/media/global/products/3xoclgdkoiofvaax/P_setting_fff_1_90_end_600.png',
                 id: 2,
+                linkUrl: 'motherboards',
             },
             {
                 title: 'RAM',
                 imageUrl: 'https://hardprice.ru/images/g/product/1700/operativnaya-pamyat-16gb-3000mhz-corsair-vengeance-rgb-pro-2x8gb-kit-cl15-ddr4-cmw16gx4m2c3000c15-2.jpg',
                 id: 3,
+                linkUrl: 'ram',
             },
             {
                 title: 'GPU',
                 imageUrl: 'https://c1.neweggimages.com/NeweggImage/ProductImage/14-137-338-V19.jpg',
                 size: 'large',
                 id: 4,
+                linkUrl: 'gpu',
             },
             {
                 title: 'CASES',
                 imageUrl: 'https://imgaz.staticbg.com/thumb/large/oaupload/banggood/images/77/37/773e21fb-c35d-401a-b3fc-2399c335a459.jpg',
                 size: 'large',
                 id: 5,
+                linkUrl: 'cases',
             },],
         }
     }
@@ -43,8 +48,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
